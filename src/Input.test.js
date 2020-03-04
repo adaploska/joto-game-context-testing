68ca8e1fda918d04b99ba5ace1f5dd7c63b2ba41
adaploska <adawagrocka@wp.pl>
2020-03-04 18:17:07 +0100

    added initial file for input component

---
+++ src/Input.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { findByTestAttr, checkProps } from "../test/testUtils";
+import Input from "./Input";
+
+/**
+ * Setup function for input component.
+ * @returns {ReactWrapper}
+ */
+const setup = (secretWord = "party") => {
+  return shallow(<Input secretWord={secretWord} />);
+};
+test("App renders without error ", () => {
+  let wrapper = setup();
+  const component = findByTestAttr(wrapper, "component-input");
+  expect(component.length).toBe(1);
+});
+test("does not throw warning with expected props", () => {
+  checkProps(Input, { secretWord: "party" });
+});
